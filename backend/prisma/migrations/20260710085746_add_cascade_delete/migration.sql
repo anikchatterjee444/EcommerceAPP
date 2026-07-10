350d56fb4b2722072aac5364a40ffb72cab79f20
anikchatterjee444 <anikchatterjee44@gmail.com>
2026-07-10 14:29:50 +0530

    feat(cart): create cart database schema

---
+++ backend/prisma/migrations/20260710085746_add_cascade_delete/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "public"."Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "public"."Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "public"."Cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
